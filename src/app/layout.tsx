57cb21e4d6da4f964260047f5e1cf6301411d486
theridwanade <theridwanade@gmail.com>
2026-04-09 21:37:15 +0100

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { JetBrains_Mono } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
+
+export const metadata: Metadata = {
+  title: "MapLocale",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn("h-full", "antialiased", "font-mono", jetbrainsMono.variable)}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
